ce46c80ac51880b3c5cf0c593619a8bad5385ba2
3Disney <victorcauzz@hotmail.com>
2022-06-18 20:55:08 -0300

    Nutmachuca

--- My project/Assets/Scripts/NutMachuca.cs
+++ My project/Assets/Scripts/NutMachuca.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class NutMachuca : MonoBehaviour
 {
+
+    public GameObject jogador;
+    private Vida vidaDoJogador;
+    private Rigidbody2D corpo;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        vidaDoJogador = jogador.GetComponent<Vida>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    /// <summary>
+    /// OnCollisionEnter is called when this collider/rigidbody has begun
+    /// touching another rigidbody/collider.
+    /// </summary>
+    /// <param name="other">The Collision data associated with this collision.</param>
+    private void OnCollisionEnter(Collision other)
+    {
+       /* if(other.gameObject.FindWithTag("Player"))
+        {
+
+        }*/
     }
 }
