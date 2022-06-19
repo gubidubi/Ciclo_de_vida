b11de0be1d5406b57522d0f08f82b93e1ccbad7e
gubidubi <98227331+gubidubi@users.noreply.github.com>
2022-06-18 21:36:03 -0300

    k1
    k1

--- My project/Assets/Scripts/Luz.cs
+++ My project/Assets/Scripts/Luz.cs
@@ -17,7 +17,7 @@ public class Luz : MonoBehaviour
     void Update()
     {
         if (cameraPrincipal.transform.position.y - transform.position.y > 24) {
-            transform.position = transform.position + (Vector3.up * (44 + Random.Range(0, 10)));
+            transform.position = transform.position + (Vector3.up * (43 + Random.Range(0, 5)));
         }
     }
     
